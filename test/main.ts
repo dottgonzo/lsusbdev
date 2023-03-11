80f6a104f4c1ee22e5bde4019a4ac22eba9b59db
dottg0n20 <dev@dariocaruso.info>
2023-03-11 16:55:18 +0100

    ..

--- test/main.ts
+++ test/main.ts
@@ -1,22 +1,19 @@
+import { expect } from "chai";
 import Usb from "../index";
-import * as chai from "chai";
-let expect = chai.expect;
 
 describe("main test", function () {
-    this.timeout(50000);
+  this.timeout(50000);
 
-    it("should return an object", function (done) {
-
-        Usb().then(function (data) {
-
-            expect(data).to.be.ok;
-            expect(data).to.be.an("array");
-            //     expect(data[0].dev).to.be.a("string");
-            done();
-        }).catch(function (err) {
-            done(err);
-        });
-
-    });
-
+  it("should return an object", function (done) {
+    Usb()
+      .then(function (data) {
+        expect(data).to.be.ok;
+        expect(data).to.be.an("array");
+        //     expect(data[0].dev).to.be.a("string");
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
 });
